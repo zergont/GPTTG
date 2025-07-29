5edcecfecb67c7d4ed911a63d0997857060e2feb
Folist <folis@gmail.com>
2025-07-29 16:39:35 +0300

    Обновление скрипта и версия проекта
    Изменена строка запуска скрипта обновления в `admin_update.py` для улучшения читаемости кода. Удалена устаревшая строка.
    
    Обновлена версия проекта в `pyproject.toml` с "1.8.9" на "1.8.10".

--- bot/handlers/admin_update.py
+++ bot/handlers/admin_update.py
@@ -90,8 +90,7 @@ async def callback_update_confirm(callback: types.CallbackQuery):
         
         # Запускаем скрипт обновления
         proc = await asyncio.create_subprocess_exec(
-            "sudo", "bash", "-c", "chmod +x", str(update_script), "&&", str(update_script), "--no-restart",
-            ### "sudo", "bash", str(update_script), "--no-restart", -СТАРАЯ СТРОКА, УБРАТЬ!
+            "sudo", "bash", "-c", f"chmod +x {update_script} && {update_script} --no-restart",
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.STDOUT,
             cwd=str(project_root)
